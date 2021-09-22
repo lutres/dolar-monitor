d98a69341be734047ecfdbe0d49a09de99b2f71a
Luis González <luismoreno785@gmail.com>
2021-09-22 09:35:08 -0400

    eur option

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import './styles.css';
 
 const date = document.querySelector('.date');
 const price = document.querySelector('.price')
+const select = document.querySelector('select');
+
 // DolarToday API
 
 fetch('https://s3.amazonaws.com/dolartoday/data.json', {
@@ -11,10 +13,21 @@ fetch('https://s3.amazonaws.com/dolartoday/data.json', {
 .then((response) => response.json())
 .then((data) => {
     // console.log(data);
-
+    
     date.innerText = data._timestamp.fecha;
     
     let priceOriginal = data.USD.promedio.toString();
     price.innerText = `${priceOriginal.substring(0, 1)}.${priceOriginal.substring(1,4)}.${priceOriginal.substring(4,7)},${priceOriginal.substring(8,10)} Bs.`
+
+    select.addEventListener('change', () => {
+        
+        select.selectedOptions[0].value === 'USD' ? priceOriginal = data.USD.promedio.toString()
+                                                : priceOriginal = data.EUR.promedio.toString();
+
+        
+        price.innerText = `${priceOriginal.substring(0, 1)}.${priceOriginal.substring(1,4)}.${priceOriginal.substring(4,7)},${priceOriginal.substring(8,10)} Bs.`
+        
+    })
+    
     
 })
